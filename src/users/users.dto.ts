fae9ffdc2dc0179987564840da094b6be88f5d40
Ricardo Barrios <rjbm29@gmail.com>
2025-02-22 20:59:53 -0500

    update user components

---
+++ src/users/users.dto.ts
@@ -0,0 +1,13 @@
+export class CreateUserDTO {
+  user_name: string;
+  nick_name: string;
+  email: string;
+  password: string;
+}
+
+export class UserListDTO {
+  id: string;
+  user_name: string;
+  nick_name: string | null;
+  email: string;
+}
