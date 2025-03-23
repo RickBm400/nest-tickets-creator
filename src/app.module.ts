0e73d95e82b25f8dff5713db5f908db7d4fcf252
Ricardo Barrios <rjbm29@gmail.com>
2025-03-23 02:39:01 -0500

    feat: event prisma model & module codebase

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,12 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { ConfigModule } from '@nestjs/config';
+import { EventsController } from './events/events.controller';
+import { EventsModule } from './events/events.module';
+import { EventsModule } from './events/events.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), AuthModule, UsersModule],
+  imports: [ConfigModule.forRoot(), AuthModule, UsersModule, EventsModule],
+  controllers: [EventsController],
 })
 export class AppModule {}
