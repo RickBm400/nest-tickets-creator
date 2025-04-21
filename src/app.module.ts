205dd882f3f389afa5caa313ef960a1e185466c1
Ricardo Barrios <rjbm29@gmail.com>
2025-04-20 22:52:39 -0500

    feat: restart from cero

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,7 @@
 import { Module } from '@nestjs/common';
-import { AuthModule } from './auth/auth.module';
-import { UsersModule } from './users/users.module';
-import { ConfigModule } from '@nestjs/config';
-import { EventsController } from './events/events.controller';
-import { EventsModule } from './events/events.module';
-import { EventsModule } from './events/events.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), AuthModule, UsersModule, EventsModule],
-  controllers: [EventsController],
+  imports: [],
+  controllers: [],
 })
 export class AppModule {}
