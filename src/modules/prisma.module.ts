8ddb6366ff423b67b81ee5c3e6d0b10fe4d65aad
Ricardo Barrios <rjbm29@gmail.com>
2025-05-02 02:07:57 -0500

    update prisma client

--- src/modules/prisma.module.ts
+++ src/modules/prisma.module.ts
@@ -1,5 +1,5 @@
 import { Global, Module } from '@nestjs/common';
-import { PrismaService } from '../infrastructure/database/prisma/prisma.service';
+import { PrismaService } from '../infrastructure/database/prisma/prismaClient';
 
 @Global()
 @Module({
